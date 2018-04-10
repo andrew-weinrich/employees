09fc78229d0a593d871f5118113a46c45223dd07
Andrew Weinrich <andrew.weinrich@gmail.com>
2018-04-10 11:08:16 -0500

    Removed bin/ directory

--- employees/bin/src/test/curl.sh
+++
@@ -1,3 +0,0 @@
-
-
-curl -X POST -H "Content-Type: application/json" http://localhost:8080/addEmployee --data '{ "FirstName":"bob", "LastName":"smith", "Title":"asdf", "Department":"qwer", "StartDate":"12/22/2012"}'
